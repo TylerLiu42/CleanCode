682e1ef6c7d9e56f28d9829f4360602c0951e990
Tyler Liu <Tyler.Liu@capco.com>
2019-11-13 13:40:00 -0500

    Restrict class scope, delete main

--- src/main/java/CalculationEngine.java
+++ src/main/java/CalculationEngine.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
 
-public class CalculationEngine {
+class CalculationEngine {
     private ArrayList<Double> roots = new ArrayList<>();
 
-    public ArrayList<Double> findRoots(double a, double b, double c) throws NoRootsException {
+    protected ArrayList<Double> findRoots(double a, double b, double c) throws NoRootsException {
         double discriminant = getDiscriminant(a, b, c);
         if (discriminant >= 0) {
             roots.add((-b + Math.sqrt(discriminant))/2*a);
--- src/main/java/InputParser.java
+++ src/main/java/InputParser.java
@@ -1,8 +1,8 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class InputParser {
-    public ArrayList<String> getCoefficients(String inputRPN) {
+class InputParser {
+    protected ArrayList<String> getCoefficients(String inputRPN) {
         String regex = "[a-z|*|+]+";
         String[] strArr = inputRPN.split(regex, 0);
         return new ArrayList<>(Arrays.asList(strArr));
